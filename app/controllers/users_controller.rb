f8f53c6b31f1e5dcec80ab621ab7f3a846170414
74dan.hamu <dangosakata.898989@gmail.com>
2020-02-28 09:09:18 +0000

    log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,4 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  
-  def index
-    @user = User.find(params[:id])
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
 
   def new
     @user = User.new
